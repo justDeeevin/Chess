15d13c6e6bae052fa3b6da0deda270273fe75b93
Devin Droddy <devin.droddy@gmail.com>
2023-02-23 23:13:13 -0500

    Update twitter card info

--- routes/index.tsx
+++ routes/index.tsx
@@ -16,15 +16,9 @@ export default function Home() {
 				<meta content="https://devinchess.deno.dev" property="og:url"/>
 				<meta content="https://devinchess.deno.dev/img/embed-image.png" property="og:image"/>
 				{/*Twitter embed info */}
-				{/* <meta name="twitter:card" content="summary"/>
+				<meta name="twitter:card" content="summary"/>
 				<meta name="twitter:title" content="Devin's Chess Project!"/>
 				<meta name="twitter:description" content="A work-in-progress chess game"/>
-				<meta name="twitter:site" content="https://devinchess.deno.dev"/>
-				<meta name="twitter:image" content="https://devinchess.deno.dev/img/embed-image.png"/> */}
-				<meta name="twitter:card" content="summary"/>
-				<meta name="twitter:site" content="@site_twitter_handle"/>
-				<meta name="twitter:title" content="Post title"/>
-				<meta name="twitter:description" content="Post description ..."/>
 			</Head>
 			<Board/>
 		</>
