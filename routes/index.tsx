33046dc743f65c73304e2938e54c8656f79d5850
Devin Droddy <devin.droddy@gmail.com>
2023-02-23 22:23:39 -0500

    Add stable discord and twitter embed info

--- routes/index.tsx
+++ routes/index.tsx
@@ -9,10 +9,18 @@ export default function Home() {
 				<title>Chess</title>
 				<link rel="stylesheet" href="css/index.css" />
 				<link rel="icon" href="img/favicon.svg" type="image/svg"/>
+
+				{/* Discord embed info */}
 				<meta content="Devin's Chess Project!" property="og:title"/>
 				<meta content="A work-in-progress chess game" property="og:description"/>
 				<meta content="https://devinchess.deno.dev" property="og:url"/>
-				<meta content="https://devinchess.deno.dev/img/favicon.svg" property="og:image"/>
+				<meta content="https://devinchess.deno.dev/img/embed-image.png" property="og:image"/>
+
+				{/*Twitter embed info */}
+				<meta content="Devin's Chess Project!" property="twitter:title"/>
+				<meta content="A work-in-progress chess game" property="twitter:description"/>
+				<meta content="https://devinchess.deno.dev" property="twitter:site"/>
+				<meta content="https://devinchess.deno.dev/img/embed-image.png" property="twitter:image"/>
 			</Head>
 			<Board/>
 		</>
