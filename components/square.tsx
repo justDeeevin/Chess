5b60f69f419bac7ed95a46aced300d2987670e48
Devin Droddy <devin.droddy@gmail.com>
2023-02-23 19:44:12 -0500

    Revert "Revert "Illegal moves no longer switch turn""
    This reverts commit 9f1b27b97229f5bae3ee8ad3500dd2cde63d1506.

--- components/square.tsx
+++ components/square.tsx
@@ -1,5 +1,6 @@
 import { FunctionComponent } from "https://esm.sh/v106/preact@10.11.0/src/index";
 import { useEffect, useState } from 'https://esm.sh/v106/preact@10.11.0/hooks'
+import { team } from "../islands/board.tsx"
 
 interface props {
     color: string
@@ -10,9 +11,11 @@ interface props {
     movePiece: (startRank: number, startFile: number, endRank: number, endFile: number) => void
     clickedPieceCoords: number[]
     isMoveLegal: (startRank: number, startFile: number, endRank: number, endFile: number) => boolean
+    turn: team
+    setTurn: (team: team) => void
 }
 
-export const Square: FunctionComponent<props> = ({ color, pieceClicked, rank, file, setPieceClicked, movePiece, clickedPieceCoords, isMoveLegal, children }) => {
+export const Square: FunctionComponent<props> = ({ color, pieceClicked, rank, file, setPieceClicked, movePiece, clickedPieceCoords, isMoveLegal, children, turn, setTurn }) => {
     useEffect(() => {
         if(pieceClicked) {
             if(!isMoveLegal(clickedPieceCoords[0], clickedPieceCoords[1], rank, file)) setLegal(false)
@@ -30,7 +33,10 @@ export const Square: FunctionComponent<props> = ({ color, pieceClicked, rank, fi
                 onClick={() => {
                     if(!pieceClicked) return
                     setPieceClicked(false)
-                    movePiece(clickedPieceCoords[0], clickedPieceCoords[1], rank, file)
+                    if(isMoveLegal(clickedPieceCoords[0], clickedPieceCoords[1], rank, file)) {
+                        movePiece(clickedPieceCoords[0], clickedPieceCoords[1], rank, file)
+                        setTurn(turn == 'white' ? 'black' : 'white')
+                    }
                 }}
             >
                 {children}
