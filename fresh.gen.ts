c26276a0e9e6de47d78fdd0909745d8c98573aca
Devin Droddy <devin.droddy@gmail.com>
2023-03-09 14:22:06 -0500

    Remove deleted route from gen file

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -5,7 +5,6 @@
 import config from "./deno.json" assert { type: "json" };
 import * as $0 from "./routes/_404.tsx";
 import * as $1 from "./routes/index.tsx";
-import * as $2 from "./routes/preview.tsx";
 import * as $$0 from "./islands/board.tsx";
 import * as $$1 from "./islands/mobileBoard.tsx";
 
@@ -13,7 +12,6 @@ const manifest = {
   routes: {
     "./routes/_404.tsx": $0,
     "./routes/index.tsx": $1,
-    "./routes/preview.tsx": $2,
   },
   islands: {
     "./islands/board.tsx": $$0,
