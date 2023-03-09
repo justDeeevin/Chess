f2aeac7462dc03aecf023026460206901d353665
Devin <90054389+ThePyroTF2@users.noreply.github.com>
2023-03-09 09:32:14 -0500

    Delete preview route
    I misunderstood how the preview deployment works lol

--- routes/preview.tsx
+++
@@ -1,9 +0,0 @@
-import { Head } from "$fresh/runtime.ts";
-
-export default function Preview() {
-    return(
-        <Head>
-            <meta http-equiv="Refresh" content="0; url='https://devinchess-040x5jt252wg.deno.dev'" />
-        </Head>
-    )
-}
