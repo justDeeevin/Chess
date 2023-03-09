9ac9f522383f78f8b0a1f4885a7969eac8565012
Devin Droddy <devin.droddy@gmail.com>
2023-03-09 09:20:37 -0500

    Add a route that redirects to the developer branch

---
+++ routes/preview.tsx
@@ -0,0 +1,9 @@
+import { Head } from "$fresh/runtime.ts";
+
+export default function Preview() {
+    return(
+        <Head>
+            <meta http-equiv="Refresh" content="0; url='https://devinchess-040x5jt252wg.deno.dev'" />
+        </Head>
+    )
+}
