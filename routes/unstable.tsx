ff1fb85bf501c7b60dca1d61e8f338636e55e1b8
Devin Droddy <devin.droddy@gmail.com>
2023-03-09 14:27:20 -0500

    Add route to unstable site

--- routes/unstable.tsx
+++ routes/unstable.tsx
@@ -3,7 +3,7 @@ import { Head } from "$fresh/runtime.ts";
 export default function Preview() {
     return(
         <Head>
-            <meta http-equiv="Refresh" content="0; url='https://devinchess-040x5jt252wg.deno.dev'" />
+            <meta http-equiv="Refresh" content="0; url='https://devinchess-unstable.deno.dev'" />
         </Head>
     )
 }
